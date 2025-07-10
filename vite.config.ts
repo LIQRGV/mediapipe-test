452d2ee0cbd9d6aa2854c3b145444f2af2aeb70e
Septian Hari <5935057+LIQRGV@users.noreply.github.com>
2025-07-10 18:32:16 +0800

    Fix mediapipe

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,67 @@
+import path from 'path'
+import fs from 'fs'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+function mediapipe_workaround() {
+  return {
+    name: 'mediapipe_workaround',
+    load(id) {
+      const MEDIAPIPE_EXPORT_NAMES = {
+        'pose.js': [
+          'POSE_LANDMARKS', 
+          'POSE_CONNECTIONS', 
+          'POSE_LANDMARKS_LEFT', 
+          'POSE_LANDMARKS_RIGHT', 
+          'POSE_LANDMARKS_NEUTRAL', 
+          'Pose',
+          'VERSION',
+        ],
+        'hands.js': [
+          'VERSION', 
+          'HAND_CONNECTIONS', 
+          'Hands', 
+        ],
+        'camera_utils.js': [
+          'Camera', 
+        ],
+        'drawing_utils.js': [
+          'drawConnectors',
+          'drawLandmarks',
+          'lerp',
+        ],
+        'control_utils.js': [
+          'drawConnectors',
+          'FPS',
+          'ControlPanel',
+          'StaticText',
+          'Toggle',
+          'SourcePicker',
+          
+          // 'InputImage', not working with this export. Is defined in index.d.ts 
+          // but is not defined in control_utils.js
+          'InputImage',
+          
+          'Slider',
+        ],
+      }
+
+      let fileName = path.basename(id);
+      if (!(fileName in MEDIAPIPE_EXPORT_NAMES)) return null
+      let code = fs.readFileSync(id, 'utf-8');
+      for (const name of MEDIAPIPE_EXPORT_NAMES[fileName]) {
+        code += `exports.${name} = ${name};`;
+      }
+      return {code};
+    },
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/mediapipe-test/",
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    mediapipe_workaround()
+  ],
 })
